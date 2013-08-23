db0b6e73808a2d8aa743dab0333d93d612237392
Mark Jayson Fuentes <markjayson.fuentes@pup.edu.ph>
2013-08-23 20:44:20 +0800

    Updated repo to reflect UI updates

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include('header.php') ?>
+<?php include('content-start.php') ?>
+<div class="page-header">
+<h1>ORYZA 2K Web</h1>
+</div>
+<p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod aliquam.</p>
+<div class="container-fluid">
+    <div>
+        <h2>Recent simulations</h2>
+    </div>
+    <div>
+        <h2>News</h2>
+    </div>
+</div>
+<p><a href="input.php">Start simulating</a></p>
+<?php include('content-end.php') ?>
+<?php include('footer.php') ?>
