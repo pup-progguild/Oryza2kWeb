70acaeb738731b26f0db2cbf3b8eaf37119561d3
Temoto-kun <allan.crisostomo@pup.edu.ph>
2013-08-27 17:25:38 +0800

    Renamed homepage.php -> oryza.php

---
+++ application/controllers/oryza.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hoshi~
+ * Date: 8/24/13
+ * Time: 5:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Oryza extends CI_Controller {
+    private function _init() {
+        $this->load->helper('url');
+    }
+
+    public function index() {
+        $data['title'] = 'Home';
+
+        $this->_init();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/content-start', $data);
+        $this->load->view('pages/home', $data);
+        $this->load->view('templates/content-end', $data);
+        $this->load->view('templates/footer', $data);
+    }
+
+    public function input() {
+        $data['title'] = 'Input';
+
+        $this->_init();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/content-start', $data);
+        $this->load->view('pages/input', $data);
+        $this->load->view('templates/content-end', $data);
+        $this->load->view('templates/footer', $data);
+    }
+
+    public function about() {
+        $data['title'] = 'About';
+
+        $this->_init();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/content-start', $data);
+        $this->load->view('pages/about', $data);
+        $this->load->view('templates/content-end', $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
