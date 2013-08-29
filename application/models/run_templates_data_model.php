c8e0d4cb543c758cd09dec3b2429f7f431c2dc83
awkwardusername <markjayson.fuentes@pup.edu.ph>
2013-08-29 14:50:41 +0800

    run_templates_data_model for testing; fetch data from database, now.

--- application/models/run_templates_data_model.php
+++ application/models/run_templates_data_model.php
@@ -51,6 +51,56 @@ class Run_templates_data_model extends CI_Model {
         $this->db->update($this->RUN_TEMPLATES_DATA_TABLE, $this, array('id' => $this->input->post('id')));
     }
 
+    public function get_a_template($label = FALSE) {
+        if($label === FALSE) {
+            $query = $this->db->get($this->RUN_TEMPLATES_DATA_TABLE);
+            return $query->result_array();
+        }
+
+        $query = $this->db->get_where($this->RUN_TEMPLATES_DATA_TABLE, 'label' , $label);
+        return $query->row_array();
+    }
+
+    public function get_template_labels($label = FALSE) {
+        if($label === FALSE) {
+            $this->db->start_cache();
+            $this->db->select('label');
+            $this->db->stop_cache();
+
+            $query = $this->db->get($this->RUN_TEMPLATES_DATA_TABLE);
+
+            return $query->result_array();
+        }
+
+        $this->db->start_cache();
+        $this->db->select('label')->where('label' , $label);
+        $this->db->stop_cache();
+
+        $query = $this->db->get($this->RUN_TEMPLATES_DATA_TABLE);
+
+        return $query->result_array();
+    }
+
+    public function get_template_labels_preset($label = FALSE) {
+        if($label === FALSE) {
+            $this->db->start_cache();
+            $this->db->select('label')->where('preset', '1');
+            $this->db->stop_cache();
+
+            $query = $this->db->get($this->RUN_TEMPLATES_DATA_TABLE);
+
+            return $query->result_array();
+        }
+
+        $this->db->start_cache();
+        $this->db->select('label')->where(array('label' => $label, 'preset' => '1'));
+        $this->db->stop_cache();
+
+        $query = $this->db->get($this->RUN_TEMPLATES_DATA_TABLE);
+
+        return $query->result_array();
+    }
+
     public function set_as_preset() {
         $this->input->post('preset');
 
