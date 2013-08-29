b0cb5b7b8aa3b89cc6fba1d8c5df7856a977074b
awkwardusername <markjayson.fuentes@pup.edu.ph>
2013-08-29 17:09:28 +0800

    weather_data_model for testing

--- application/models/weather_data_model.php
+++ application/models/weather_data_model.php
@@ -12,7 +12,6 @@
 
 class Weather_data_model extends CI_Model {
     var $WEATHER_DATA_TABLE = 'weather_data';
-
     var $country = '';
     var $year = '';
     var $data = '';
@@ -26,7 +25,7 @@ class Weather_data_model extends CI_Model {
     public function add() {
         $this->input->post('country');
         $this->input->post('year');
-        $this->input->data('data');
+        $this->input->post('data');
 
         $this->db->insert($this->WEATHER_DATA_TABLE, $this);
     }
@@ -34,8 +33,37 @@ class Weather_data_model extends CI_Model {
     public function update() {
         $this->input->post('country');
         $this->input->post('year');
-        $this->input->data('data');
+        $this->input->post('data');
 
         $this->db->update($this->WEATHER_DATA_TABLE, $this, array('id' => $this->input->post('id')));
+    }
+
+    public function get_weather_data($country = FALSE, $year = FALSE) {
+        if($country === FALSE && $year === FALSE) {
+            $this->db->start_cache();
+            $this->db->select('country', 'year');
+            $this->db->stop_cache();
+
+            $query = $this->db->get($this->WEATHER_DATA_TABLE);
+
+            return $query->result_array();
+
+        } elseif ($country === FALSE) {
+
+            $query = $this->db->get_where($this->WEATHER_DATA_TABLE, array('year' => $year));
+            return $query->result_array();
+
+        } elseif ($year === FALSE) {
+
+            $query = $this->db->get_where($this->WEATHER_DATA_TABLE, array('country' => $country));
+            return $query->result_array();
+        }
+
+        $query = $this->db->get_where($this->WEATHER_DATA_TABLE, array(
+            'country' => $country,
+            'year' => $year
+        ));
+
+        return $query->results_array();
     }
 }
