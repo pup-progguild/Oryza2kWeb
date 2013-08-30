0b7177daeb3e99b3d3127894a442d30fc3fe62f1
awkwardusername <markjayson.fuentes@pup.edu.ph>
2013-08-30 17:49:54 +0800

    run_templates_data table altered, added new attribute, description and file_prefix

--- application/models/run_templates_data_model.php
+++ application/models/run_templates_data_model.php
@@ -14,10 +14,11 @@ class Run_templates_data_model extends CI_Model {
     var $RUN_TEMPLATES_DATA_TABLE = 'run_templates_data';
 
     var $label = '';                // refers to 'variety'. set as 'label' because it pertains to a set of input for a run
+    var $file_prefix = '';          // the file prefix used to prepend to the *.exp and *.crp files for input
     var $description = '';          // 'description' of template
     var $control_dat = '';          // control.dat
-    var $experiment_data_dat = '';  // experiment_data.exp
-    var $crop_data_dat = '';        // crop_data.crp
+    var $experiment_data_dat = '';  // experiment_data.exp = {$file_prefix}.exp
+    var $crop_data_dat = '';        // crop_data.crp = {$file_prefix}.crp
     var $reruns_dat = '';           // reruns.dat
     var $preset = '';               // if a template is regarded as a 'variety' by IRRI.
 
@@ -33,6 +34,7 @@ class Run_templates_data_model extends CI_Model {
      */
     public function add() {
         $this->input->post('label');
+        $this->input->post('file_prefix');
         $this->input->post('description');
         $this->input->post('control_dat');
         $this->input->post('experiment_data_dat');
@@ -49,6 +51,7 @@ class Run_templates_data_model extends CI_Model {
      */
     public function update() {
         $this->input->post('label');
+        $this->input->post('file_prefix');
         $this->input->post('description');
         $this->input->post('control_dat');
         $this->input->post('experiment_data_dat');
@@ -60,9 +63,9 @@ class Run_templates_data_model extends CI_Model {
     }
 
     /*
-     * @function         get_a_template
+     * @function         get_template
      * @description      returns template data for desired $label. if not specified, all table data is returned.
-     * @params           $label
+     * @params           $label         The short description of a template
      */
     public function get_template($label = FALSE) {
         if($label === FALSE) {
@@ -70,14 +73,14 @@ class Run_templates_data_model extends CI_Model {
             return $query->result_array();
         }
 
-        $query = $this->db->get_where($this->RUN_TEMPLATES_DATA_TABLE, 'label' , $label);
+        $query = $this->db->get_where($this->RUN_TEMPLATES_DATA_TABLE, array('label' => $label));
         return $query->row_array();
     }
 
     /*
      * @function         get_template_labels
      * @description      returns the labels for all presets. if not specified, all template labels are returned.
-     * @params           $preset
+     * @params           $preset        Specifies if template is a preset by IRRI
      */
     public function get_template_labels($preset = FALSE) {
         if($preset === FALSE) {
@@ -101,7 +104,7 @@ class Run_templates_data_model extends CI_Model {
     /*
      * @function         get_template_description
      * @description      returns the descriptions for all presets. if not specified, all template descriptions are returned.
-     * @params           $preset
+     * @params           $preset        Specifies if template is a preset by IRRI
      */
     public function get_template_descriptions($preset = FALSE) {
         if($preset === FALSE) {
@@ -121,6 +124,31 @@ class Run_templates_data_model extends CI_Model {
         $query = $this->db->get($this->RUN_TEMPLATES_DATA_TABLE);
 
         return $query->result_array();
+    }
+    /*
+     * @function         get_template_file_prefix
+     * @description      returns the file prefix for all templates on specified label. if not specified, all template
+     *                      file prefixes are returned.
+     * @params           $label         The short description of a template
+     */
+    public function get_template_file_prefix($label = FALSE) {
+        if($label === FALSE) {
+            $this->db->start_cache();
+            $this->db->select('file_prefix');
+            $this->db->stop_cache();
+
+            $query = $this->db->get($this->RUN_TEMPLATES_DATA_TABLE);
+
+            return $query->result_array();
+        }
+
+        $this->db->start_cache();
+        $this->db->select('file_prefix');
+        $this->db->stop_cache();
+
+        $query = $this->db->get_where($this->RUN_TEMPLATES_DATA_TABLE, array('label' => $label) );
+
+        return $query->row_array();
     }
 
     /*
