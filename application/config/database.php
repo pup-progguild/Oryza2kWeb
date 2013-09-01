8353ce8dc3fcf27133293634b05ce91bb6a632b9
Temoto-kun <kiiroifuriku@hotmail.com>
2013-09-01 10:18:17 +0800

    Database asduff

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';   // TODO change to localhost for deployment
-$db['default']['username'] = 'oryza';
-$db['default']['password'] = 'dev_oryza2kweb';
+//$db['default']['username'] = 'oryza';
+//$db['default']['password'] = 'dev_oryza2kweb';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
 $db['default']['database'] = 'oryza';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
